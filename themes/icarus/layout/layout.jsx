f3fd862eac1502b2c402e8089b69d28ac2068c23
HuangHe <huanghe@zuifuli.com>
2020-06-30 20:34:04 +0800

    解决语雀跨域问题

--- themes/icarus/layout/layout.jsx
+++ themes/icarus/layout/layout.jsx
@@ -15,6 +15,7 @@ module.exports = class extends Component {
         const columnCount = Widgets.getColumnCount(config.widgets);
 
         return <html lang={language ? language.substr(0, 2) : ''}>
+        <meta name="referrer" content="no-referrer" />
             <Head env={env} site={site} config={config} helper={helper} page={page} />
             <body class={`is-${columnCount}-column`}>
                 <Navbar config={config} helper={helper} page={page} />
